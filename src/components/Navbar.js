e9ed5c3b1289efad5635e5e082168bd7fcfb4112
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-06-23 23:22:23 -0400

    adding style to navbar as buttons - hovering and what not

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,11 @@ function Navbar(props){
         <nav>
             <h3 className="nav--logo_text">Nishant Patel</h3>
             <div className='nav-items'>
-                <span><Link to="/" style={{ textDecoration: 'none' }}><h4 className="nav--title">About</h4></Link></span>
-                <span><Link to="/experience" style={{ textDecoration: 'none' }}><h4 className="nav--title">Experience</h4></Link></span>
-                <span><Link to="/skills" style={{ textDecoration: 'none' }}><h4 className="nav--title">Skills</h4></Link></span>
-                <span><Link to="/academics" style={{ textDecoration: 'none' }}><h4 className="nav--title">Academics</h4></Link></span>
-                <span><h4 className="nav--title"><a href={pdf} target="_blank" style={{ textDecoration: 'none' }}>Resume</a></h4></span>
+                <span id='music'><Link to="/" style={{ textDecoration: 'none' }}><h4 className="nav--title">About</h4></Link></span>
+                <span id='music'><Link to="/experience" style={{ textDecoration: 'none' }}><h4 className="nav--title">Experience</h4></Link></span>
+                <span id='music'><Link to="/skills" style={{ textDecoration: 'none' }}><h4 className="nav--title">Skills</h4></Link></span>
+                <span id='music'><Link to="/academics" style={{ textDecoration: 'none' }}><h4 className="nav--title">Academics</h4></Link></span>
+                <span id='music'><h4 className="nav--title"><a href={pdf} target="_blank" style={{ textDecoration: 'none' }}>Resume</a></h4></span>
             </div>
             {cubes}
         </nav>
