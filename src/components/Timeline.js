7f389282927ac5454973e5a732e6ad5271398c73
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-07-10 20:17:12 -0400

    uoft courses

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,41 +1,12 @@
 import React from 'react'
 import { Chrono } from "react-chrono";
 
-function Timeline(){
-  const items = [
-    { title: 'Timeline title 1', cardTitle: 'Card Title 1' },
-    { title: 'Timeline title 2', cardTitle: 'Card Title 2' },
-    { title: 'Timeline title 2', cardTitle: 'Card Title 2' },
-  ];
-
+function Timeline(props){
     return (
       <div>
         <div style={{ width: "100%", height: "calc(100%-1em)", 'margin-top':'10px', 'margin-bottom':'100px'}}>
-          <Chrono items={items} mode="VERTICAL_ALTERNATING" hideControls={true}>
-            <div>
-              <div>
-                <p style={{color:'black'}}>Lorem Ipsum. Lorem Ipsum. Lorem Ipsum</p>
-              </div>
-              <div>
-                <img src="https://loremflickr.com/320/240" />
-              </div>
-            </div>
-            <div>
-              <div>
-                <p style={{color:'black'}}>Lorem Ipsum. Lorem Ipsum. Lorem Ipsum</p>
-              </div>
-              <div>
-                <img src="https://loremflickr.com/320/240" />
-              </div>
-            </div>
-            <div>
-              <div>
-                <p style={{color:'black'}}>Lorem Ipsum. Lorem Ipsum. Lorem Ipsum</p>
-              </div>
-              <div>
-                <img src="https://loremflickr.com/320/240" />
-              </div>
-            </div>
+          <Chrono items={props.items} mode="VERTICAL_ALTERNATING" hideControls={true} theme={{cardBgColor:'#21222A', cardDetailsColor:'#DEEBF8'}} useReadMore={true}>
+
           </Chrono>
         </div>
       </div>
