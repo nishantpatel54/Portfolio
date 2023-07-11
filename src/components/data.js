f3b1924d20f255f61c54d2b226e7b5d74b015bae
Nishant Patel <60250103+np5499@users.noreply.github.com>
2023-07-11 00:09:31 -0400

    moving timeline data and temporarily changing skills css

---
+++ src/components/data.js
@@ -0,0 +1,40 @@
+const data= {
+    courses : [
+        { title: 'Sept 2018 - Dec 2018', 
+        cardTitle: 'Fall 2018 University of Toronto',
+        timelineContent: 
+        <div className='experience-content'>
+          <ul>
+            <li>CSCA08H3 Introduction to Computer Science I</li>
+            <li>CSCA67H3 Discrete Mathematics</li>
+            <li>MATA31H3 Calculus I for Mathematical Sciences</li>
+            <li>BIOA01H3 Life on Earth: Unifying Principles</li>
+            <li>CHMA10H3 Introductory Chemistry I: Structure and Bonding</li>
+          </ul>
+      </div>},
+        { title: 'Jan 2019 - Apr 2019', 
+        cardTitle: 'Winter 2019 University of Toronto',
+        timelineContent : 
+        <div className='experience-content'>
+          <ul>
+            <li>CSCA48H3 Introduction to Computer Science II</li>
+            <li>MATA22H3 Linear Algebra I for Mathematical Sciences</li>
+            <li>MATA37H3 Calculus II for Mathematical Sciences</li>
+            <li>BIOA02H3 Life on Earth: Form, Function and Interactions</li>
+            <li>CHMA11H3 Introductory Chemistry II</li>
+          </ul>
+        </div>},
+        { title: 'May 2019 - July 2019 ', 
+        cardTitle: 'Summer 2019 University of Toronto',
+        timelineContent : 
+        <div className='experience-content'>
+          <ul>
+            <li>STAB57H3 Introduction to Statistics</li>
+            <li>CSCB36H3 Introduction to the Theory of Computation</li>
+            <li>MATB24H3 Linear Algebra II</li>
+          </ul>
+        </div>}
+      ]
+}
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -7,25 +7,14 @@ import Intro from './components/Intro.js'
 import Timeline from './components/Timeline';
 import Skills from './components/Skills';
 import reportWebVitals from './reportWebVitals';
+import data from './components/data.js';
 function App() {
-
-  const experiences = [
-    { title: 'Sept 2018 - Dec 2018', 
-    cardTitle: 'Fall 2018 University of Toronto', 
-    cardDetailedText: ["- CSCA08H3 Introduction to Computer Science I", "- CSCA67H3 Discrete Mathematics", "- MATA31H3 Calculus I for Mathematical Sciences", '- BIOA01H3 Life on Earth: Unifying Principles', '- CHMA10H3 Introductory Chemistry I: Structure and Bonding']},
-    { title: 'Jan 2019 - Apr 2019', 
-    cardTitle: 'Winter 2019 University of Toronto',
-    cardDetailedText : ['- CSCA48H3 Introduction to Computer Science II', '- MATA22H3 Linear Algebra I for Mathematical Sciences', '- MATA37H3 Calculus II for Mathematical Sciences', '-BIOA02H3 Life on Earth: Form, Function and Interactions', '- CHMA11H3 Introductory Chemistry II: Reactions and Mechanisms']},
-    { title: 'May 2019 - July 2019 ', 
-    cardTitle: 'Summer 2019 University of Toronto',
-    cardDetailedText: ['- STAB57H3 Introduction to Statistics', '- CSCB36H3 Introduction to the Theory of Computation', '- MATB24H3 Linear Algebra II']}
-  ];
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Navbar/>}>
         <Route index element={<Intro />} />
-          <Route path="experience" element={<Timeline items={experiences}/>} />
+          <Route path="experience" element={<Timeline items={data.courses}/>} />
           <Route path="skills" element={<Skills />} />
           <Route path="academics" element={<Timeline />} />
         </Route>
